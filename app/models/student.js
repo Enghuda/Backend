c9aaec370227122eabd66efbeda2716d9a5cd6c7
maimonah95 <mim95s@hotmail.com>
2020-03-12 17:48:21 +0300

    add Schema

---
+++ app/models/student.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+// Define Student Schema
+const studentSchema = new mongoose.Schema({
+  FirstName: { type: String, required: true },
+  LastName: { type: String, required: true },
+  ID: Number,
+  Gender: String,
+  DOB: Date,
+  subject: [{ type: Schema.Types.ObjectId, ref: "Subject" }]
+});
+
+// Compile our Model based on the Schema
+const Student = mongoose.model("Student", studentSchema);
+
+// Export our Model for use
+module.exports = Student;
